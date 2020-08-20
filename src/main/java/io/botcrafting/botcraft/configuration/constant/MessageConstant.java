59d406a68a462f8cc59a86d731fddc648cc08afc
Rafael Baptista <rafael.baptista@dextra-sw.com>
2020-08-19 21:37:34 -0300

    send message / hellow world

---
+++ src/main/java/io/botcrafting/botcraft/configuration/constant/MessageConstant.java
@@ -0,0 +1,8 @@
+package io.botcrafting.botcraft.configuration.constant;
+
+public class MessageConstant {
+    public static String CTHULHU_AWAKEN = "Ph'nglui mglw'nafh Cthulhu R'lyeh wagah'nagl fhtagn.";
+    public static String WORLD_CTHULHU = "cthulhu";
+    public static String BOTCRAFT_NAME = "botcraft";
+    public static String ANSWER_BOTCRAFT_MENTION = "Você é um mero mortal, %s. Como ousa chamar o meu nome? Minha hora ainda não chegou.";
+}
