bb80a5d401a74e99e0801b42d2a78611e2b3c88c
Thiago <mullerthiago8@gmail.com>
2020-09-13 11:40:57 -0300

    Adding feedback to the user

--- src/main/java/io/botcrafting/botcraft/infra/api/telegram/TelegramApi.java
+++ src/main/java/io/botcrafting/botcraft/infra/api/telegram/TelegramApi.java
@@ -3,6 +3,7 @@ package io.botcrafting.botcraft.infra.api.telegram;
 import static io.botcrafting.botcraft.configuration.constant.UrlConstant.API_TELEGRAM_BASE_URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +26,13 @@ public class TelegramApi implements MessageSenderService{
 	public void sendMessageText(long chatId, String messageText) throws HttpClientErrorException{
 		String url = API_TELEGRAM_BASE_URL + System.getenv("botToken") + "/sendMessage";
 		log.info("I'm calling rest template on: " + url + " for sending message text");
-		restTemplate.postForObject(url, new TelegramMessageText(chatId, messageText), String.class);
+		try {
+			restTemplate.postForObject(url, new TelegramMessageText(chatId, messageText), String.class);
+		}catch (HttpClientErrorException http) {
+			restTemplate.postForObject(url, new TelegramMessageText(chatId, "Desculpe, não consegui entender o que você disse!!!"), String.class);
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, http.getMessage());
+		}
+		
 		
 	}
 
@@ -34,7 +41,13 @@ public class TelegramApi implements MessageSenderService{
 		String url = API_TELEGRAM_BASE_URL + System.getenv("botToken") + "/sendPhoto";
 		log.info("I'm calling rest template on: " + url + " for sending message photo");
 		log.info("Photo URL: " + imageUrl);
-        restTemplate.postForObject(url, new TelegramMessagePhoto(chatId, imageUrl, messageText), String.class);
+		try {
+			restTemplate.postForObject(url, new TelegramMessagePhoto(chatId, imageUrl, messageText), String.class);
+		}catch (HttpClientErrorException http) {
+			restTemplate.postForObject(url, new TelegramMessageText(chatId, "Desculpe, não consegui entender o que você disse!!!"), String.class);
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, http.getMessage());
+		}
+        
 		
 	}
 	
@@ -43,7 +56,13 @@ public class TelegramApi implements MessageSenderService{
 		String url = API_TELEGRAM_BASE_URL + System.getenv("botToken") + "/sendVideo";
 		log.info("I'm calling rest template on: " + url + " for sending message gif");
 		log.info("Gif URL: " + gifUrl);
-		restTemplate.postForObject(url, new TelegramMessageVideo(chatId, gifUrl), String.class);
+		try {
+			restTemplate.postForObject(url, new TelegramMessageVideo(chatId, gifUrl), String.class);
+		}catch (HttpClientErrorException http) {
+			restTemplate.postForObject(url, new TelegramMessageText(chatId, "Desculpe, não consegui entender o que você disse!!!"), String.class);
+			throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, http.getMessage());
+		}
+		
 		
 	}
 }
